9096a1267a051b8aaba7d67e84f860974dbb31b4
efuraimuJS <efuraimu.js@gmail.com>
2022-06-01 03:09:41 +0300

    Correction Basic & Advance Tasks

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-        last = number % -10
+    last = number % -10
 else:
-        last = number % 10
-        if last > 5:
-                print("Last digit of {0:d} is {1:d}".format(number, last), end=' ')
-                    print("and is greater than 5")
-        elif last == 0:
-                print("Last digit of {0:d} is {1:d} and is 0".format(number, last))
-        elif last < 6 and last != 0:
-                print("Last digit of {0:d} is {1:d}".format(number, last), end=' ')
-                    print("and is less than 6 and not 0")
+    last = number % 10
+if last > 5:
+    print("Last digit of {0:d} is {1:d}".format(number, last), end=' ')
+    print("and is greater than 5")
+elif last == 0:
+    print("Last digit of {0:d} is {1:d} and is 0".format(number, last))
+elif last < 6 and last != 0:
+    print("Last digit of {0:d} is {1:d}".format(number, last), end=' ')
+    print("and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-        firts = number / 10
-            second = number % 10
+    firts = number / 10
+    second = number % 10
 
-                if firts < second and firts != second and number != 89:
-                            print("{:02d}, ".format(number), end='')
-                                if number == 89:
-                                            print("{}".format(number))
+    if firts < second and firts != second and number != 89:
+        print("{:02d}, ".format(number), end='')
+    if number == 89:
+        print("{}".format(number))
