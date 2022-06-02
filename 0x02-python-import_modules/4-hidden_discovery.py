be07fd08c9ea810d8a95e424f5ff6fe5a51a28c4
efuraimuJS <96207346+efuraimuJS@users.noreply.github.com>
2022-06-03 02:47:08 +0300

    Basic & Advance Task - correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print(i)
+    all_dir = dir(hidden_4)
+    avoid = "__"
+    for i in range(0, len(all_dir)):
+        if avoid not in all_dir[i]:
+            print(all_dir[i])
