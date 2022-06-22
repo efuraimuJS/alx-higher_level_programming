05a650d8c36bb7d5fe25be8ca3dcafe955bb2113
efuraimuJS <96207346+efuraimuJS@users.noreply.github.com>
2022-06-22 04:15:12 +0300

    advance tasks

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,83 +1,98 @@
 #!/usr/bin/python3
+"""Class for Node"""
 
 
 class Node:
-    """Node of a singly linked list.
-    Private instance attribute: data:
-        - property def data(self)
-        - property setter def data(self, value)
-    Private instance attribute: next_node:
-        - property def next_node(self)
-        - property setter def next_node(self, value)
-    Instantiation with data and next_node.
+    """ defines a node of a singly linked list
+        Attributes:
+            data (int): data
+            next_node (Node, optional): node
     """
 
     def __init__(self, data, next_node=None):
-        """Initializes the data of the node."""
+        """Initialize Node
+        args:
+            data (int): data stored in node
+            next_node (Node): next node
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Retrieves the data from the node."""
+        """data getter
+        returns:
+            data (int)
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
-        """Sets the data into a node."""
-        if not isinstance(value, int):
+        """data setter
+        args:
+            value (int): value to set
+        returns:
+            None
+        """
+        if type(value) != int:
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """Retrieves the next_node."""
+        """data getter
+        returns:
+            data (int)
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """Sets the next_node."""
+        """data setter
+        args:
+            value (Node): value to set
+        returns:
+            None
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """ Singly linked list.
-    Private instance attribute: head.
-    Simple instantiation.
-    Public instance method: def sorted_insert(self, value).
+    """Singly linked list class
     """
 
     def __init__(self):
-        """Initializes the linked list."""
-        self.head = None
-
-    def __str__(self):
-        """For the print statement in the main file."""
-        my_str = ""
-        node = self.head
-        while node:
-            my_str += str(node.data)
-            my_str += '\n'
-            node = node.next_node
-        return my_str[:-1]
+        """Initialize linked list"""
+        self.__head = None
 
     def sorted_insert(self, value):
-        """Inserts a node in a sorted linked list."""
-        new_node = Node(value)
+        """insert node in coorect sorted position
+        args:
+            value (int): value for new node
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
 
-        if self.head is None:
-            self.head = new_node
-            return
+            new.next_node = tmp.next_node
+            tmp.next_node = new
 
-        if value < self.head.data:
-            new_node.next_node = self.head
-            self.head = new_node
-            return
-
-        node = self.head
-        while node.next_node and node.next_node.data < value:
-            node = node.next_node
-        new_node.next_node = node.next_node
-        node.next_node = new_node
+    def __str__(self):
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,82 +1,106 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
-    Private instance attribute: position:
-        - property def position(self)
-        - property setter def position(self, value)
-    Instantiation with optional size and optional position.
-    Public instance method: def area(self).
-    Public instance method: def my_print(self).
-    """
+"""square class main"""
+
+
+class Square():
+    """square class"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes the data."""
-        self.__size = size
-        self.__position = position
+        """ Instance of class Square
+    Arguments:
+        @size: size of side of square"""
 
-    def __str__(self):
-        """Str method for print from main module."""
-        my_str = ""
-        if self.__size == 0:
-            return ''
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        elif (len(position) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(position) != tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif ((type(position[0]) != int) or (type(position[1]) != int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (position[0] < 0) or (position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(position) is not tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            my_str += '\n' * self.__position[1]
-            for i in range(0, self.__size):
-                my_str += ' ' * self.__position[0]
-                my_str += '#' * self.__size
-                my_str += '\n'
-            return my_str[:-1]
+            self.__size = size
+            self.__position = position
+
+    def area(self):
+        """ area of square
+        Return:
+                area of square."""
+        return self.__size ** 2
 
     @property
     def size(self):
-        """Retrieves the size."""
+        """ getter of size
+    Return:
+            value of size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size to a value."""
-        if not isinstance(value, int):
+        """ setter of the size
+    Arguments:
+        value: value of size"""
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
-        """Retrieves the position."""
+        """ getter of position
+    Return:
+            value of position"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Sets the position to a value."""
-        if not isinstance(value, tuple) or len(value) != 2:
+        """ setter of the position
+    Arguments:
+        value: value of postion"""
+        if (type(value) != tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
+        elif (len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
+        elif ((type(value[0]) != int) or (type(value[1]) != int)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def area(self):
-        """Returns the current square area."""
-        return self.__size ** 2
+        elif (value[0] < 0) or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(position) is not tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def my_print(self):
-        """Prints to stdout the square with the character #,
-        at the position given by the position attribute.
-        """
-        if self.__size == 0:
-            print()
+        """ square made using the character #
+            or a blank line if @size == 0"""
+        if (self.size == 0):
+            print("")
         else:
-            for y in range(0, self.__position[1]):
+            for x in range(self.position[1]):
                 print()
-            for i in range(0, self.__size):
-                for x in range(0, self.__position[0]):
-                    print(" ", end="")
-                for j in range(0, self.__size):
-                    print("#", end="")
-                print()
-            return ''
+            for i in range(self.size):
+                print((self.position[0] * " ") + ("#" * self.size))
+
+    def __str__(self):
+        """ square made using the character #
+        """
+        temp = ""
+        if (self.size == 0):
+            return temp
+        else:
+            for i in range(self.position[1]):
+                temp += "\n"
+            for i in range(self.size):
+                if (i < self.size - 1):
+                    temp += (self.position[0] * " ") + ("#" * self.size) + "\n"
+                else:
+                    temp += (self.position[0] * " ") + ("#" * self.size)
+        return temp
