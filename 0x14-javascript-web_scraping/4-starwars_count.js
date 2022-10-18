535f2ff682e2ec664ad09c2578301a718e35ab2b
efuraimujs <efuraimu.js@gmail.com>
2022-10-18 23:54:38 +0300

    adding tasks 0..6

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(body).results;
+    for (const i in results) {
+      const characters = results[i].characters;
+      for (const j in characters) {
+        if (characters[j].includes('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
